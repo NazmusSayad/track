cd332b88b43a84654a1938d526ad837b344f5f62
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-07-26 11:41:06 +0000

    M package.json  M src/App.tsx  M yarn.lock ?? test.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,62 @@
 import Wrapper from '$layouts/Wrapper'
+import calculate from 'cords-distance'
+import { useEffect, useRef, useState } from 'react'
+
+const defaultCoords = { altitude: 0, altitudeAccuracy: 0 }
 
 const App = () => {
-  return (
-    <Wrapper style={{ height: '90%', display: 'grid', placeContent: 'center' }}>
-      <h1 style={{ textAlign: 'center', fontSize: '7vw' }}>Hello world!</h1>
-    </Wrapper>
-  )
+  const [cords, setCords] = useState({})
+  const prevCordsRef = useRef<any>()
+  const [distance, setDistance] = useState({ distance: 0, min: 0, max: 0 })
+
+  useEffect(() => {
+    function handleCoords({ ...coords }: any) {
+      coords.altitude ??= 0
+      coords.altitudeAccuracy ??= 0
+
+      const prevCoords = { ...(prevCordsRef.current ?? coords) }
+      prevCordsRef.current = coords
+
+      const distance = calculate(prevCoords, coords)
+      setDistance((prev) => ({
+        distance: prev.distance + distance.distance,
+        min: prev.min + distance.min,
+        max: prev.max + distance.max,
+      }))
+    }
+
+    const id = navigator.geolocation.watchPosition(
+      ({
+        coords: {
+          latitude,
+          longitude,
+          accuracy,
+          altitude,
+          altitudeAccuracy,
+          speed,
+          heading,
+        },
+      }) => {
+        handleCoords({
+          latitude,
+          longitude,
+          accuracy,
+          altitude,
+          altitudeAccuracy,
+          speed,
+          heading,
+        })
+      },
+      () => {},
+      { enableHighAccuracy: true }
+    )
+
+    return () => {
+      navigator.geolocation.clearWatch(id)
+    }
+  }, [])
+
+  return JSON.stringify(distance)
 }
 
 export default App
---
+++ test.js
@@ -0,0 +1,8 @@
+import calculate from 'cords-distance'
+
+console.log(
+  calculate(
+    { latitude: 38.8976, longitude: -77.0366 },
+    { latitude: 39.9496, longitude: -75.1503 }
+  )
+)
